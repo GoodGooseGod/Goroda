e49b110da486acaaa09856f302e0c8d566019b58
Katerina <snopkova05@mail.ru>
2024-12-08 01:42:53 +0700

    Update UserInterface.h
    Интерфейс заголовок

--- UserInterface.h
+++ UserInterface.h
@@ -1,1 +1,16 @@
-﻿
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class UserInterface {
+public:
+    void instructions();
+    void menu();
+    string writePlayerName(int playerNumber);
+    string writeCurrentCity();
+    void citiesVector(vector<string>& cities);
+    void result(string& winner);
+};
