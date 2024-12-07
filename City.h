2a8df0c6b0444dfd74648b872c80f264169fc217
kazahkazah <valerapinterest05@gmail.com>
2024-12-08 01:47:11 +0700

    Класс "города"
    будут вопросы или предложения - пишите. я спать

--- City.h
+++ City.h
@@ -1,2 +1,27 @@
 ﻿#pragma once
-	
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <fstream> 
+#include <iterator>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+class CityList
+{
+public:
+	vector <string> current_citys;  // города, которые находятся в игре(игроки их называли)
+	vector <string> possible_citys; // все возможные города 
+
+
+	CityList(string filename);
+	void add_new_city(string new_city);
+	void unic_check(string some_city);
+	void is_real_check(string some_city);
+	string last_city();
+	vector <string> current_citys(); 
+	~CityList();
+};
