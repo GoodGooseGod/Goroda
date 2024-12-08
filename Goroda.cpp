c549ee5f0f0610b1609a13c6679fab808630cdb4
Shemelov Anton <shemelov03@mail.ru>
2024-12-09 03:47:18 +0700

    Class Game

--- Goroda.cpp
+++ Goroda.cpp
@@ -1,20 +1,248 @@
-﻿// Goroda.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#pragma once
 #include <iostream>
+#include <windows.h>
+#include "City.h"
+#include "City.cpp"
+#include "UserInterface.h"
+#include "UserInterface.cpp"
+#include "Player.h"
+#include "Player.cpp"
+#include "ComputerPlayer.h"
+#include "ComputerPlayer.cpp"
 
-int main()
-{
-    std::cout << "Привет всем!!!! С НГ!!!";
+#define border  cout << "--------------------------------------------------" << endl
+#define setConsoleColor(a) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), a);
+
+using namespace std;
+
+const string file = "CityList.txt";
+const int red = 12, white = 7;
+const int tryConst = 5;
+
+class Game {
+    CityList CList;     // список городов
+    UserInterface UI;   // пользовательский интерфейс
+    Player *P1, *P2;   // игроки
+    unsigned int turns = 0; // количество ходов
+    bool gameNotEnded = true;
+    char compPlayer;
+public:
+    Game(); // инициализация игры
+
+    void startGame(char compPlayer);   // старт игры
+    void stopGame();    // остановка игры
+    void endGame(string winner) { UI.result(winner); gameNotEnded = false; };     // конец игры
+    char turn(char letter);
+
+    void error(string text);
+};
+
+
+Game::Game() : UI(), CList(file), P1(new Player()) { // конструктор
+    UI.instructions();
+
+    while (!P2) {
+        UI.menu();
+        cin >> compPlayer;
+        switch (compPlayer) {
+        case '1': P2 = new Player(); break;
+        case '2': P2 = new ComputerPlayer("conputer", CList.get_possible_citys()); break;
+        default: error("Неверный номер режима!"); break;
+        }
+    }
+    border;
+    P1->setName(UI.writePlayerName(1)); cin;
+    if (compPlayer == '1') P2->setName(UI.writePlayerName(2));
+
+    border;
+    compPlayer = NULL;
+    while (!compPlayer) {
+        cout << "Выберите игрока, который будет ходить первым(1 или 2): ";
+        cin >> compPlayer;
+        if (compPlayer != '1' && compPlayer != '2') {
+            error("Неверный номер игрока!");
+            compPlayer = NULL;
+        }
+    }
+    startGame(compPlayer);
+};
+
+void Game::error(string text) { // вывод текста красным шрифтом в случае ошибки
+    border;
+    setConsoleColor(red);
+    cout << text << endl;
+    setConsoleColor(white);
+    border;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void Game::startGame(char compPlayer) { // начало игры
+    border;
+    cout << "Начало игры!" << " Ход игрока ";
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    char last = NULL;   // первый ход без буквы
+    int tries = tryConst;
+    if (compPlayer == '1') {
+        cout << P1->getName() << '!' << endl;
+        while (!last && gameNotEnded && tries > 0) {     // ввод города и проверка корректности значения
+            string city;
+            cout << "Осталось попыток: " << tries << '.' << endl;
+            tries--;
+            city = UI.writeCurrentCity();
+            if (city == "стоп") {
+                stopGame();
+                continue;
+            }
+            if (CList.unic_check(city) && CList.is_real_check(city)) {
+                CList.add_new_city(city);
+                last = city[city.length() - 1];
+                if (last == 'ь' || last == 'ъ' || last == 'ы') last = city[city.length() - 2];
+            }
+            else error("Данный город был назван или отсутствует в списке возможных городов!");
+        }
+        if (tries == 0) endGame(P2->getName());
+    }
+    else {
+        if (ComputerPlayer* cp = dynamic_cast<ComputerPlayer*>(P2)) {
+            cout << P2->getName() << '!' << endl;
+            string city;
+            city = P2->chooseCity("ЙЦУКЕНГШЩЗХФВАПРОЛДЖЭЯЧСМИТБЮЁ"[rand() % 30]);
+            cout << "Игрок " << P2->getName() << " назвал город: " << city << endl;
+            CList.add_new_city(city);
+
+            last = city[city.length() - 1];
+            if (last == 'ь' || last == 'ъ' || last == 'ы') last = city[city.length() - 2];
+        }
+        else {
+            cout << P2->getName() << '!' << endl;
+            while (!last && gameNotEnded && tries > 0) {     // ввод города и проверка корректности значения
+                string city;
+                cout << "Осталось попыток: " << tries << '.' << endl;
+                tries--;
+                city = UI.writeCurrentCity();
+                if (city == "стоп") {
+                    stopGame();
+                    continue;
+                }
+                if (CList.unic_check(city) && CList.is_real_check(city)) {
+                    CList.add_new_city(city);
+                    last = city[city.length() - 1];
+                    if (last == 'ь' || last == 'ъ' || last == 'ы') last = city[city.length() - 2];
+                }
+                else error("Данный город был назван или отсутствует в списке возможных городов!");
+            }
+            if (tries == 0) endGame(P1->getName());
+        }
+    }
+    turns++;
+    
+
+    while (gameNotEnded) { // основная игра
+        last = turn(last);
+        turns++;
+    }
+}
+
+char Game::turn(char letter) { // ход игрока
+    string city;
+    char last;
+    int tries = tryConst;
+    letter -= 32; // перевод буквы в верхний регистр
+    if (letter == 'ё') letter = 'Ё';
+
+    border;
+    cout << "Ход #" << turns << ". Ход игрока ";
+    if ((turns + compPlayer) % 2 != 1) {
+        cout << P2->getName() << '!' << endl;
+        cout << "Последний названный город: " << CList.last_city() << "." << endl << "Требуется назвать город на букву ";
+        setConsoleColor(red);
+        cout << letter << endl;
+        setConsoleColor(white);
+
+        if (ComputerPlayer* cp = dynamic_cast<ComputerPlayer*>(P2)) {
+            city = P2->chooseCity(letter);
+            cout << "Игрок " << P2->getName() << " назвал город: " << city << endl;
+            CList.add_new_city(city);
+
+            last = city[city.length() - 1];
+            if (last == 'ь' || last == 'ъ' || last == 'ы') last = city[city.length() - 2];
+            return last;
+        }
+        else {
+            char last = NULL;
+            while (!last && gameNotEnded && tries > 0) { // ввод города и проверка корректности значения
+                cout << "Осталось попыток: " << tries << '.' << endl;
+                tries--;
+                city = UI.writeCurrentCity();
+                if (city == "стоп") {
+                    stopGame();
+                    continue;
+                }
+                if (CList.unic_check(city) && CList.is_real_check(city) && city[0] == letter) {
+                    CList.add_new_city(city);
+
+                    last = city[city.length() - 1];
+                    if (last == 'ь' || last == 'ъ' || last == 'ы') last = city[city.length() - 2];
+                    return last;
+                }
+                else error("Данный город был назван или отсутствует в списке возможных городов!");
+            }
+            if (tries == 0) endGame(P1->getName());
+        }
+    }
+    else {
+        cout << P1->getName() << '!' << endl;
+        cout << "Последний названный город: " << CList.last_city() << "." << endl << "Требуется назвать город на букву ";
+        setConsoleColor(red);
+        cout << letter << endl;
+        setConsoleColor(white);
+
+        char last = NULL;
+        while (!last && gameNotEnded && tries > 0) { // ввод города и проверка корректности значения
+            cout << "Осталось попыток: " << tries << '.' << endl;
+            tries--;
+            city = UI.writeCurrentCity();
+            if (city == "стоп") {
+                stopGame();
+                continue;
+            }
+            if (CList.unic_check(city) && CList.is_real_check(city) && city[0] == letter) {
+                CList.add_new_city(city);
+
+                last = city[city.length() - 1];
+                if (last == 'ь' || last == 'ъ' || last == 'ы') last = city[city.length() - 2];
+                return last;
+            }
+            else error("Данный город был назван или отсутствует в списке возможных городов!");
+        }
+        if (tries == 0) endGame(P2->getName());
+    }
+}
+
+void Game::stopGame() {
+    error("Игра на паузе. Для продолжения напишите 'продолжить.\n\
+Для того, чтобы вывести список использованных городов напишите 'список'.\n\
+Для того, чтобы завершить игру напишите 'завершить' ");
+    string str;
+    while (true) {
+        cin >> str;
+        if (str == "список") {
+            border;
+            UI.citiesVector(CList.get_current_citys());
+        }
+        if (str == "продолжить") {
+            border;
+            return;
+        }
+        if (str == "завершить") {
+            gameNotEnded = false;
+            return;
+        }
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+        
+    Game game;
+}
